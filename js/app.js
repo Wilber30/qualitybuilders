bdaa5900fd158e6cc038120ce5a197132c9d90d7
Joseph Whittle <Joewhittle101@gmail.com>
2021-09-30 15:18:56 +0100

    Bug fixes
    - Added working carousel for testimonials.
    - added working carousel for banner.

--- js/app.js
+++ js/app.js
@@ -9,20 +9,23 @@ $(document).ready(function(){
         speed: 300,
         autoplay: true,
         arrows: true
+    });
+  });
 
-        
+  $(document).ready(function(){
+    $('.testimonials-carousel-inner').slick({
+        infinite: true,
+        speed: 300,
+        autoplay: true,
+        dots: true
+    });
+  });
 
-        //the following is how slick handles media queries
-        /*
-        responsive: [
-            {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
-              }
-              */
+  $(document).ready(function(){
+    $('.banner-carousel-inner').slick({
+        infinite: true,
+        speed: 800,
+        autoplay: true,
+        arrows: true
     });
   });
