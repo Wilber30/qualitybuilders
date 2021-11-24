0d7dcd90cb69daf99120b7fbabc3f81768c69411
Joseph Whittle <Joewhittle101@gmail.com>
2021-11-24 02:06:52 +0000

    Updated some bits + bug fix
    - Fixed some validation.
    - Ensured it will work for cpanel.

--- js/contact-form-functions.js
+++ js/contact-form-functions.js
@@ -1,48 +1,66 @@
-//the map on the contact page
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mymap);
-
-L.marker([51.5, -0.09]).addTo(mymap);
-
-L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
 
 
-//the contact form functionality 
+//the contact form validation functionality 
+function isFormValid() {
 
-var fields = {};
-document.addEventListener("DOMContentLoaded", function () {
-    fields.firstName = document.getElementById('fname');
-    fields.lastName = document.getElementById('lname');
-    fields.emailAddress = document.getElementById('eAddress');
-    fields.phoneNumber = document.getElementById('pNumber');
-    fields.message = document.getElementById('subject');
-    fields.gdprCheckBox = document.getElementById('gdprCheckbox');
-})
+    //run through and grab all the data in the form
+    const fName = document.getElementById("fName");
+    const lName = document.getElementById("lName");
+    const eAddress = document.getElementById("eAddress");
+    const pNumber = document.getElementById("pNumber");
+    const message = document.getElementById("message");
+    const gdpr = document.getElementById("gdpr");
 
-//checks whether a field value is empty or not
-function isNotEmpty(value) {
-    if (value == null || typeof value == 'undefined') return false;
-    return (value.length > 0);
+    const submitButton = document.getElementById("submitButton");
+
+
+
+    //array so that I can loop through the input elements instead of doing them all individually as, loads are just text fields tbh
+    const inputElements = [];
+    inputElements.push(fName, lName, eAddress, pNumber, message, gdpr);
+
+    const checkedInputElements = [];
+
+    //Current client side validation does email and empty fields, ill more as I have time and find approprirate regex
+    for (let index = 0; index < inputElements.length; index++) {
+        //establish iterator element varible
+        let curElement = inputElements[index];
+
+        if(curElement.value.length != 0) {
+            checkedInputElements.push(curElement);
+            //is not empty
+            curElement.style.borderColor = "green";
+
+            if((curElement.name == "eAddress") && (validateEmail(curElement.value))) {
+                checkedInputElements.push(curElement);
+                curElement.style.borderColor = "green";
+            } else if(curElement.name == "eAddress")  {
+                checkedInputElements.splice(curElement ,1);
+                curElement.style.borderColor = "red";
+            }
+        }
+        else {
+            //is empty field
+            checkedInputElements.splice(curElement ,1);
+            curElement.style.borderColor = "red";
+        }
+    }
+
+
+    //now when they are all good, allow the button to be pressed
+    if (checkedInputElements.length < 6) {
+        submitButton.disabled = true;
+    } else {
+        submitButton.disabled = false;
+        //show a error message or smth
+    }
+
+    //UNDO THIS, WHEN YOU WORK OUT HOW TO ACCESS CPANEL SQLDATABASES
+    submitButton.disabled = true;
+
 }
+
+
 
 //checks whether a field value is is a number or not
 function isNumber(num) {
@@ -54,63 +72,16 @@ function gdprCheck(value) {
     return (value.checked);
 }
 
-//just a filler function ready for recaptcha but there isnt any recaptcha in reality
-function checkRecaptcha() {
-
+//regex function that returns a bool as to whether to the email placed in the params is valid
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
 
-//checks whether the field value is a properly formatted email address
-function isEmail(email) {
-    const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return regex.test(String(email).toLowerCase());
-}
 
+//checks phone number against regex
 function isPhoneNumber(number) {
     const regex = /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/;
     return regex.test(number);
 }
 
-//runs the actual validation
-function fieldValidation(field, validationFunction) {
-    if (field == null) return false;
-
-    let isFieldValid = validationFunction(field.value)
-    if (!isFieldValid) {
-        field.className = 'placeholderRed';
-    } else {
-        field.className = '';
-    }
-
-    return isFieldValid;
-}
-
-
-//Combines all the validation into one final function
-function isValid() {
-    var valid = true;
-
-    valid &= fieldValidation(fields.firstName, isNotEmpty);
-    valid &= fieldValidation(fields.lastName, isNotEmpty);
-    valid &= fieldValidation(fields.emailAddress, isNotEmpty);
-    valid &= fieldValidation(fields.emailAddress, isEmail);
-    valid &= fieldValidation(fields.phoneNumber, isNotEmpty);
-    valid &= fieldValidation(fields.phoneNumber, isNumber)
-    valid &= fieldValidation(fields.message, isPhoneNumber);
-    valid &= fieldValidation(fields.gdprCheckBox, gdprCheck);
-    //recaptcha would go below
-
-    return valid;
-}
-
-isFormValid = false;
-function isFormValid () {
-    if(isValid() == true) {
-        //if true then allow the form to be submitted
-        document.getElementById("good-input").classList.remove("hidden-default");
-    }
-    else {
-        //show some text saying its wrong 
-        document.getElementById("bad-input").classList.remove("hidden-default");
-    }
-    
-}
